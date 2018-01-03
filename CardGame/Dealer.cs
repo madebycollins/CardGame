d6d869583bb7e4fe8d19a9309fd0c4e426642f40
Collins Zimba <collinszim@gmail.com>
2018-01-03 19:53:13 +0200

    Initial program structure

---
+++ CardGame/Dealer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    class Dealer
+    {
+
+
+        public void Deal()
+        {
+            
+        }
+
+        public void Shuffle(String[][] cards )
+        {
+
+        }
+
+        public void Shuffle(List<String> cards)
+        {
+            
+        }
+    }
+}
---
+++ CardGame/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Welcomes the User to a game of cards
+            Console.WriteLine("Welcome to the Card Came");
+
+            //Instantiates the Dealer
+
+        }
+    }
+}
